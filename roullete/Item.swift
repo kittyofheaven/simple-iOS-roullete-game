4e5ca38f488f7a5f9e19954c9ca975ba593b675f
Hazel Handrata <71081063+kittyofheaven@users.noreply.github.com>
2024-01-23 18:20:56 +0700

    Initial Commit

---
+++ roullete/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  roullete
+//
+//  Created by Hazel Handrata on 23/01/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ roullete/roulleteApp.swift
@@ -0,0 +1,32 @@
+//
+//  roulleteApp.swift
+//  roullete
+//
+//  Created by Hazel Handrata on 23/01/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct roulleteApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
